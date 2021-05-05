b371fabe251c56bca102f2218816276e625a3c72
DINCER & CO <70865813+BSCPGROUPHOLDINGSLLC@users.noreply.github.com>
2021-05-05 04:28:25 -0400

    Create iphunter.java

---
+++ iphunter.java
@@ -0,0 +1,43 @@
+  
+package ipHunter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ipHunter extends Frame implements ActionListener {
+    private static final long serialVersionUID = 2L;
+    JTextField tf;
+    JLabel l;
+    JButton b;
+
+    ipHunter() {
+        tf = new JTextField();
+        tf.setBounds(50, 50, 150, 20);
+        l = new JLabel();
+        l.setBounds(50, 100, 250, 20);
+        b = new JButton("Find IP");
+        b.setBounds(50, 150, 95, 30);
+        b.addActionListener(this);
+        add(b);
+        add(tf);
+        add(l);
+        setSize(400, 400);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        try {
+            String host = tf.getText();
+            String ip = java.net.InetAddress.getByName(host).getHostAddress();
+            l.setText("IP of " + host + " is: " + ip);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ipHunter();
+    }
+}
